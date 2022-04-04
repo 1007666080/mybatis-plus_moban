9095c7d40e256cc6739b9c617950e284ee42ae99
wdq <1007666080@qq.com>
2022-04-04 13:37:27 +0800

    cc

---
+++ src/main/java/com/x/generate/beans/aaa.java
@@ -0,0 +1,8 @@
+package com.x.generate.beans;
+
+/**
+ * @Author Lenovo
+ * @Date 2022-04-04 13:28
+ */
+public class aaa {
+}
---
+++ src/main/java/com/x/generate/beans/bbb.java
@@ -0,0 +1,8 @@
+package com.x.generate.beans;
+
+/**
+ * @Author Lenovo
+ * @Date 2022-04-04 13:31
+ */
+public class bbb {
+}
